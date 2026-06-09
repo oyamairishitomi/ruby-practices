52490fa3f112e27d2d19e96741dcaee34373d869
oyamairishitomi <h.tezuka0707@gmail.com>
2026-06-09 10:47:22 +0900

    Refactor bowling score using Enumerable#sum

--- 03.bowling/bowling.rb
+++ 03.bowling/bowling.rb
@@ -3,22 +3,21 @@
 
 balls = ARGV[0].split(',').map { |b| b == 'X' ? 10 : b.to_i }
 
-score = 0
-i = 0
-
-9.times do
-  if balls[i] == 10
-    score += 10 + balls[i + 1] + balls[i + 2]
-    i += 1
-  elsif balls[i] + balls[i + 1] == 10
-    score += 10 + balls[i + 2]
-    i += 2
-  else
-    score += balls[i] + balls[i + 1]
-    i += 2
-  end
+frame_starts = (1..9).inject([0]) do |frame_indices, _|
+  i = frame_indices.last
+  next_start = balls[i] == 10 ? i + 1 : i + 2
+  frame_indices + [next_start]
 end
 
-score += balls[i..].sum
+score = frame_starts.first(9).sum do |i|
+  if balls[i] == 10
+    balls[i, 3].sum
+  elsif balls[i] + balls[i + 1] == 10
+    balls[i, 3].sum
+  else
+    balls[i] + balls[i + 1]
+  end
+end
+score += balls[frame_starts.last..].sum
 
 puts score
