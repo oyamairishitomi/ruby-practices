3c580d5980dc7cca9da9d37b659712bf22b7b155
oyamairishitomi <h.tezuka0707@gmail.com>
2026-06-11 10:52:07 +0900

    要素を揃える修正を追加

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -4,12 +4,12 @@ def fetch_files(path)
   Dir.entries(path).reject { |file| file.start_with?('.') }.sort
 end
 
-def display_files(files, max)
+def display_files(files, max, item_len_max)
   height = (files.length.to_f / max).ceil
   height.times do |row|
     max.times do |col|
       item = files[row + col * height]
-      print "#{item}　" if item
+      print "#{item.to_s.ljust(item_len_max, ' ')}　" if item
     end
     puts ''
   end
@@ -17,4 +17,7 @@ end
 
 path = ARGV[0] || '.'
 files = fetch_files(path)
-display_files(files, 3)
+file_num = 3
+lengths = files.map(&:length)
+item_len_max = lengths.max
+display_files(files, file_num, item_len_max)
