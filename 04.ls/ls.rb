49ea8b40e091e9ac974ecf5e6896417599a1dc6a
oyamairishitomi <h.tezuka0707@gmail.com>
2026-07-03 11:08:53 +0900

    余計なファイルを削除

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -5,15 +5,19 @@ require 'etc'
 
 MAX_COLUMN_NUMBER = 3
 PERMISSION_TABLE = ['---', '--x', '-w-', '-wx', 'r--', 'r-x', 'rw-', 'rwx'].freeze
-COLUMN_KEYS = %i[nlink owner group size].freeze
 
-def get_files(path, all)
-  entries = Dir.entries(path).sort
+def get_files(path)
+  Dir.entries(path).reject { |file| file.start_with?('.') }
+end
 
-  if all
-    entries
+def type_char(stat)
+  case stat.ftype
+  when 'directory'
+    'd'
+  when 'link'
+    'l'
   else
-    entries.reject { |file| file.start_with?('.') }
+    '-'
   end
 end
 
@@ -23,16 +27,10 @@ def permission_string(stat)
 end
 
 def fetch_files(path, options)
-  files = get_files(path, options['a'])
-  files = files.reverse if options['r']
-  files
-end
-
-def type_char(stat)
-  case stat.ftype
-  when 'directory' then 'd'
-  when 'link' then 'l'
-  else '-'
+  if options['r']
+    get_files(path).sort.reverse
+  else
+    get_files(path).sort
   end
 end
 
@@ -48,34 +46,14 @@ def display_files(files)
   end
 end
 
-def column_width(file_details)
-  COLUMN_KEYS.map do |k|
-    file_details.map { |detail| detail[k].length }.max
-  end
-end
-
-def display_file_details(file_details)
-  puts "total #{file_details.sum { |detail| detail[:blocks] } / 2}"
-
-  nlink_width, owner_width, group_width, size_width = column_width(file_details)
-
-  file_details.each do |detail|
-    nlink = detail[:nlink].rjust(nlink_width)
-    owner = detail[:owner].rjust(owner_width)
-    group = detail[:group].rjust(group_width)
-    size = detail[:size].rjust(size_width)
-    puts "#{detail[:permission]} #{nlink} #{owner} #{group} #{size} #{detail[:mtime]} #{detail[:name]}"
-  end
-end
-
-options = ARGV.getopts('arl')
+options = ARGV.getopts('rl')
 path = ARGV[0] || '.'
 files = fetch_files(path, options)
 
 if options['l']
   file_details = files.map do |file|
     full_path = File.join(path, file)
-    stat = File.lstat(full_path)
+    stat = File.stat(full_path)
     {
       permission: "#{type_char(stat)}#{permission_string(stat)}",
       nlink: stat.nlink.to_s,
@@ -87,7 +65,21 @@ if options['l']
       blocks: stat.blocks
     }
   end
-  display_file_details(file_details)
+
+  puts "total #{file_details.sum { |d| d[:blocks] } / 2}"
+
+  nlink_width = file_details.map { |d| d[:nlink].length }.max
+  owner_width = file_details.map { |d| d[:owner].length }.max
+  group_width = file_details.map { |d| d[:group].length }.max
+  size_width = file_details.map { |d| d[:size].length }.max
+
+  file_details.each do |d|
+    nlink = d[:nlink].rjust(nlink_width)
+    owner = d[:owner].rjust(owner_width)
+    group = d[:group].rjust(group_width)
+    size = d[:size].rjust(size_width)
+    puts "#{d[:permission]} #{nlink} #{owner} #{group} #{size} #{d[:mtime]} #{d[:name]}"
+  end
 else
   display_files(files)
 end
